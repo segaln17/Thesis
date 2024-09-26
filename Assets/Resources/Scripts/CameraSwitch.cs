88f78f0440fbd0dc35255c85a33f4ea17c0ec5df
segaln17 <segaln17@gmail.com>
2024-09-26 16:37:02 -0400

    Controller work!!! It works now + camera rotation with movement

--- Assets/Resources/Scripts/CameraSwitch.cs
+++ Assets/Resources/Scripts/CameraSwitch.cs
@@ -9,6 +9,8 @@ public class CameraSwitch : MonoBehaviour
 
     public CinemachineVirtualCamera thirdPerson;
 
+    //public static CameraSwitch cameraState;
+
     public bool firstPersonPOVOn = true;
     // Start is called before the first frame update
     void Start()
--- Assets/Resources/Scripts/SimpleController.cs
+++ Assets/Resources/Scripts/SimpleController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.iOS;
 
 public class SimpleController : MonoBehaviour
 {
@@ -8,6 +11,27 @@ public class SimpleController : MonoBehaviour
     Rigidbody rb;
     public float force = 5.0f;
     public float rotationSpeed = 100f;
+
+    //THE THINGS BELOW ARE AN ATTEMPT AT PARENTING CONTROLS TO WHERE THE CAMERA IS FACING
+    //mouse sensitivity
+    public float sensX = 1f;
+    public float sensY = 1f;
+
+    public float xRotation;
+    public float yRotation;
+
+    public Transform orientation;
+    
+    public float mouseX;
+
+    public float mouseY;
+
+    private CameraSwitch cameraController;
+    private void Awake()
+    {
+        cameraController = GetComponent<CameraSwitch>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,28 +41,57 @@ public class SimpleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        if (cameraController.firstPersonPOVOn)
+        {
+            FirstPerson();
+        }
+
+        if (cameraController.firstPersonPOVOn == false)
+        {
+            ThirdPerson();
+        }
+        
+        
+        //rotate cam and orientation:
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
         
         //controller using input to get directions
         
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
+        //new attempt at movement:
+        Vector3 movementDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         
+        rb.AddForce(movementDirection.normalized * force * 2f, ForceMode.Force);
+
+
+
+        /*
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
 
 
         rb.AddForce(movementDirection * force);
         //transform.Translate(movementDirection * force * Time.deltaTime, Space.World);
+        */
 
-
+        /*
         //making sure that the player transform is smoothly following the direction of movement
         if (movementDirection != Vector3.zero)
         {
             //transform.forward =
                 //movementDirection; //the players transform, will now face the direction that the new vector3 is facing but only if it isn't stationary
 
-         
+
         Quaternion
             toRotation =
                 Quaternion.LookRotation(movementDirection,
@@ -46,8 +99,20 @@ public class SimpleController : MonoBehaviour
         transform.rotation =
             Quaternion.RotateTowards(transform.rotation, toRotation,
                 rotationSpeed * Time.deltaTime); //rotate towards is to rotate towards the desired direction
+                }
 
+*/
     }
 
+    public void FirstPerson()
+    {
+        //for first person controller, min and max control lookroom (min is up, max is down)
+        //for a third person controller, we would just have to make the clamp values be 0, 0
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+
+    public void ThirdPerson()
+    {
+        xRotation = Mathf.Clamp(xRotation, 0, 0);
     }
 }
