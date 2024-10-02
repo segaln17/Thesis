0ccda84cfe6869aabe48e4705f3aefd1ec7571c2
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-10-02 17:13:47 -0400

    part2 wednesday

---
+++ Assets/Resources/Scripts/SceneSwitchScript.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchScript : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
