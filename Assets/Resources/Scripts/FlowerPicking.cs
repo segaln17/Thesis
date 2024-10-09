06ae7d45a85947c959f4c1f26f87ea628bc21a27
segaln17 <segaln17@gmail.com>
2024-10-08 22:05:32 -0400

    added second flower, added collecting UI and button to transition to painting scene

--- Assets/Resources/Scripts/FlowerPicking.cs
+++ Assets/Resources/Scripts/FlowerPicking.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FlowerPicking : MonoBehaviour
 {
@@ -13,11 +16,15 @@ public class FlowerPicking : MonoBehaviour
 
     public List<GameObject> BigFlowerInventory = new List<GameObject>();
     public List<GameObject> SmallFlowerInventory = new List<GameObject>();
+
+    public GameObject flowerText;
+    public GameObject doneCollectingUI;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        doneCollectingUI.SetActive(false);
+        flowerText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -55,25 +62,50 @@ public class FlowerPicking : MonoBehaviour
         Debug.Log("Entered");
         Debug.Log(other);
         //AddItem();
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
         if (other.gameObject.tag == "BigFlower") 
         {
             Debug.Log("thing is here");
             if (BigFlowerInventory.Count <= 1){
-                BigFlowerInventory.Add(other.gameObject);
-                Debug.Log(BigFlowerInventory);
-                Destroy(other.gameObject);
+                flowerText.SetActive(true);
+                if (Input.GetMouseButton(0))
+                {
+                    BigFlowerInventory.Add(other.gameObject);
+                    Debug.Log(BigFlowerInventory);
+                    //changing to setactive.false so it doesn't get destroyed from the list:
+                    other.gameObject.SetActive(false);
+                    flowerText.SetActive(false);
+                    //Destroy(other.gameObject);
+                }
             }
         }
-    
         
         if (SmallFlowerInventory.Count <= 2)
         {
             if (other.gameObject.tag == "SmallFlower")
             {
-                SmallFlowerInventory.Add(other.gameObject);
-                Debug.Log(SmallFlowerInventory);
-                Destroy(other.gameObject);
+                flowerText.SetActive(true);
+                if (Input.GetMouseButton(0))
+                {
+                    SmallFlowerInventory.Add(other.gameObject);
+                    Debug.Log(SmallFlowerInventory);
+                    //changing to setactive.false so it doesn't get destroyed from the list:
+                    other.gameObject.SetActive(false);
+                    flowerText.SetActive(false);
+                    //Destroy(other.gameObject);
+                }
             }
         }
+            
+        //show the done collecting UI
+        if (BigFlowerInventory.Count >=1 || SmallFlowerInventory.Count >= 2)
+        {
+            doneCollectingUI.SetActive(true);
+        }
     }
+
 }
