bac868c01a47f0af6d1246385003d0749fa15c3e
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-10-24 14:07:29 -0400

    songscriptedit

--- Assets/Resources/Scripts/SongScript.cs
+++ Assets/Resources/Scripts/SongScript.cs
@@ -27,10 +27,19 @@ public class SongScript : MonoBehaviour
     public AudioClip hum12;
 
     public GameObject fighter;
+
+    public humRegister state;
+    public enum humRegister
+    {
+        high,
+        mid,
+        low
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        state = humRegister.mid;
     }
 
     // Update is called once per frame
@@ -43,68 +52,43 @@ public class SongScript : MonoBehaviour
 
         if (sheetActive)
         {
+            Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
             sheetMusic.SetActive(true);
             fighter.GetComponent<SimpleController>().enabled = false;
+            
+            if(Input.GetAxis("Mouse ScrollWheel") > 0f && Input.GetAxis("Mouse ScrollWheel") < 0.05f && state == humRegister.mid)
+            {
+                state = humRegister.high;
+            }
+            
+            if(Input.GetAxis("Mouse ScrollWheel") > 0f && Input.GetAxis("Mouse ScrollWheel") < 0.05f && state == humRegister.low)
+            {
+                state = humRegister.mid;
+            }
+            
+            if(Input.GetAxis("Mouse ScrollWheel") < 0 && Input.GetAxis("Mouse ScrollWheel") > -0.05f && state == humRegister.mid)
+            {
+                state = humRegister.low;
 
-            if(Input.GetAxis("Mouse ScrollWheel") == 0)
-            {
-                if (Input.GetKeyDown(KeyCode.W))
-                {
-                    SongAudio.PlayOneShot(hum01);
-                }
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    SongAudio.PlayOneShot(hum02);
-                }
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    SongAudio.PlayOneShot(hum03);
-                }
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    SongAudio.PlayOneShot(hum04);
-                }
-                
             }
-            if(Input.GetAxis("Mouse ScrollWheel") > 0)
+            if(Input.GetAxis("Mouse ScrollWheel") < 0 && Input.GetAxis("Mouse ScrollWheel") > -0.05f && state == humRegister.high)
             {
-                if (Input.GetKeyDown(KeyCode.W))
-                {
-                    SongAudio.PlayOneShot(hum12);
-                }
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    SongAudio.PlayOneShot(hum11);
-                }
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    SongAudio.PlayOneShot(hum09);
-                }
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    SongAudio.PlayOneShot(hum10);
-                }
-                
+                state = humRegister.mid;
             }
-            if(Input.GetAxis("Mouse ScrollWheel") < 0)
+
+            if (state == humRegister.mid)
             {
-                if (Input.GetKeyDown(KeyCode.W))
-                {
-                    SongAudio.PlayOneShot(hum05);
-                }
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    SongAudio.PlayOneShot(hum06);
-                }
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    SongAudio.PlayOneShot(hum08);
-                }
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    SongAudio.PlayOneShot(hum07);
-                }
-                
+                midRange();
+            }
+            
+            if (state == humRegister.low)
+            {
+                lowRange();
+            }
+            
+            if (state == humRegister.high)
+            {
+                highRange();
             }
             
         }
@@ -113,5 +97,66 @@ public class SongScript : MonoBehaviour
             sheetMusic.SetActive(false);
             fighter.GetComponent<SimpleController>().enabled = true;
         }
+    }
+
+    void midRange()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SongAudio.PlayOneShot(hum01);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SongAudio.PlayOneShot(hum02);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SongAudio.PlayOneShot(hum03);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SongAudio.PlayOneShot(hum04);
+        }
+    }
+
+    void lowRange()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SongAudio.PlayOneShot(hum12);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SongAudio.PlayOneShot(hum11);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SongAudio.PlayOneShot(hum09);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SongAudio.PlayOneShot(hum10);
+        }
+    }
+
+    void highRange()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SongAudio.PlayOneShot(hum05);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SongAudio.PlayOneShot(hum06);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SongAudio.PlayOneShot(hum08);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SongAudio.PlayOneShot(hum07);
+        }
+        
     }
 }
