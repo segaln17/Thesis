ef3e6ec3e37f887bf4bf73958d85537dcfa1b852
segaln17 <segaln17@gmail.com>
2024-11-26 17:49:40 -0500

    yarn issue fixed SOMEHOW??? test scene for timelines triggered through yarn

---
+++ Assets/Resources/Scripts/TimelineTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Yarn.Unity;
+using Input = UnityEngine.Windows.Input;
+
+public class TimelineTest : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    [YarnCommand("timelineGo")]
+    public void GoTimeline()
+    {
+        timeline.Play();
+        Debug.Log("cube");
+    }
+}
