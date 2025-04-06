b42b77a2a386a9236ececd210723c6ce1751d0ce
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2025-04-06 18:10:32 -0400

    signtesting

---
+++ Assets/Resources/Scripts/Random/TextAnimEvent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAnimEvent : MonoBehaviour
+{
+    public GameObject threedTextObject;
+
+    public void activateText()
+    {
+        threedTextObject.GetComponent<Collider>().enabled = true;
+        threedTextObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    public void turnTextOff()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+}
