9119673dc88bb759b547def5406c363a66008a7c
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2025-04-21 19:29:15 -0400

    act 1 and begin sword zone

---
+++ Assets/Resources/Scripts/Intro/ActOneAppear.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActOneAppear : MonoBehaviour
+{
+    public GameObject act1appear;
+    public AudioSource soundEffects;
+    public AudioClip introSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(actOneAppear());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator actOneAppear()
+    {
+        yield return new WaitForSeconds(35f);
+        act1appear.SetActive(true);
+        soundEffects.PlayOneShot(introSound);
+        yield return new WaitForSeconds(1f);
+        StopCoroutine(actOneAppear());
+    }
+}
