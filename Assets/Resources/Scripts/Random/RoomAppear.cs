3fc22d90c5ec8f33d0e367a9f1003a9bdf1eb115
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2025-05-13 18:33:36 -0400

    chorus attempt

--- Assets/Resources/Scripts/Random/RoomAppear.cs
+++ Assets/Resources/Scripts/Random/RoomAppear.cs
@@ -18,6 +18,7 @@ public class RoomAppear : MonoBehaviour
     private float pForce;
     private float pFallForce;
     public bool roomOn;
+    public bool inRoom;
     public bool notinroom;
 
     public PlayableDirector roomTurnTimeline;
@@ -30,6 +31,7 @@ public class RoomAppear : MonoBehaviour
         pFallForce = playerPhoebe.GetComponent<SimpleController>().fallingforce;
         notinroom = true;
         roomOn = false;
+        inRoom = false;
     }
 
     // Update is called once per frame
@@ -54,10 +56,11 @@ public class RoomAppear : MonoBehaviour
             playerPhoebe.GetComponent<SimpleController>().fallingforce = 0;
 
             playerCamScriptHolder.GetComponent<PlayerCam>().yRotation = Mathf.Clamp(playerCamScriptHolder.GetComponent<PlayerCam>().yRotation, -200, -145);
-            playerCamScriptHolder.GetComponent<PlayerCam>().xRotation = Mathf.Clamp(playerCamScriptHolder.GetComponent<PlayerCam>().xRotation, 0, 50);
+            playerCamScriptHolder.GetComponent<PlayerCam>().xRotation = Mathf.Clamp(playerCamScriptHolder.GetComponent<PlayerCam>().xRotation, 0, 30);
+            inRoom = true;
             //mainCamera.cullingMask 
-            //float tempY = phoebeOrientation.transform.localEulerAngles.y;
-            //tempY = Mathf.Clamp(phoebeOrientation.transform.localEulerAngles.y, -175f, -195f);
+           // float tempY = phoebeOrientation.transform.localEulerAngles.y;
+           // tempY = Mathf.Clamp(tempY, -175f, -195f);
             Debug.Log("roomsequence");
 
         }
@@ -78,7 +81,8 @@ public class RoomAppear : MonoBehaviour
     [YarnCommand ("turnOffChorusRoom")]
     public void turnoffRoom()
     {
-        StopAllCoroutines();
+        //StopAllCoroutines();
+        StopCoroutine(WaitChorus());
         StartCoroutine("WaitNoRoom");
         Room.SetActive(false);
         roomOn = false;
@@ -87,7 +91,19 @@ public class RoomAppear : MonoBehaviour
         playerPhoebe.GetComponent<SimpleController>().fallingforce = pFallForce;
         playerCamScriptHolder.GetComponent<PlayerCam>().xRotation = Mathf.Clamp(playerCamScriptHolder.GetComponent<PlayerCam>().xRotation, -90, 90);
         playerCamScriptHolder.GetComponent<PlayerCam>().yRotation = Mathf.Clamp(playerCamScriptHolder.GetComponent<PlayerCam>().yRotation, -360, 360);
-
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("cam01");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("cam02");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Default");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Ground");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Player");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Water");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Ignore Raycast");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("InventoryItem");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("PuzzlePieces");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("UI");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("PlayerWalkIgnore");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("TransparentFX");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("NPCSprites");
         //mainCamera.cullingMask = 1 << LayerMask.NameToLayer("Everything");
 
         Debug.Log("noroomsequence");
@@ -98,7 +114,7 @@ public class RoomAppear : MonoBehaviour
         yield return new WaitForSeconds(1f);
         //phoebeOrientation.transform.LookAt(Room.transform);
         //roomCameraAnimator.SetBool("RoomIsOn", true);
-        roomTurnTimeline.Play();
+        //roomTurnTimeline.Play();
         yield return new WaitForSeconds(1f);
         if (FindObjectOfType<DialogueRunner>().IsDialogueRunning == false)
         {
@@ -106,7 +122,7 @@ public class RoomAppear : MonoBehaviour
         }
         Debug.Log("dialogue would play");
         yield return new WaitForSeconds(2f);
-        mainCamera.cullingMask = 1 << LayerMask.NameToLayer("Room");
+        
    
     }
 
---
+++ Assets/Resources/Scripts/Random/roomoverlay.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomoverlay : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject phoebeOrientation;
+
+    public GameObject Room;
+    // Start is called before the first frame update
+    private void OnBecameVisible()
+    {
+        mainCamera.cullingMask = 1 << LayerMask.NameToLayer("Room");
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("cam01");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("cam02");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Default");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Ground");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Player");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Water");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("Ignore Raycast");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("InventoryItem");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("PuzzlePieces");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("UI");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("PlayerWalkIgnore");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("TransparentFX");
+        mainCamera.cullingMask |= 1 << LayerMask.NameToLayer("NPCSprites");
+    }
+}
